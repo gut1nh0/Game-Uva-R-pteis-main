a4849c3d862a9dc2bed47e0c89482cfce7180667
Augusto Ferrari <augusto.ferrari.senai404@gmail.com>
2025-04-17 16:33:51 -0300

    17/04

--- JS/colecoes.js
+++ JS/colecoes.js
@@ -1,2 +1,20 @@
-const btnTodos = document.querySelector('[data-id= "1"]')
+const todos = document.querySelectorAll('.container')
 
+function abrirConteudo(idSelecionado, botaoClicado) {
+    todos.forEach(div => {
+        if (div.id === idSelecionado) {
+            div.classList.remove("desativado");
+            div.classList.add("ativo");
+        } else {
+            div.classList.remove("ativo");
+            div.classList.add("desativado");
+        }
+    })
+}
+    
+function abrirTodos(){
+    todos.forEach(div =>{
+        div.classList.remove("desativado");
+        div.classList.add("ativo");
+    })
+}
